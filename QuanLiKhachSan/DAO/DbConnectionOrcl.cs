0af409b1258262d05d2e225ca4133abb258939f8
Dung2k3 <tmdung2k3@gmail.com>
2024-11-24 17:46:33 +0700

    main | Add oracle conn

---
+++ QuanLiKhachSan/DAO/DbConnectionOrcl.cs
@@ -0,0 +1,67 @@
+﻿using QuanLiKhachSan.Class;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Oracle.ManagedDataAccess.Client;
+
+namespace QuanLiKhachSan.DAO
+{
+    class DbConnectionOrcl
+    {
+        public static OracleConnection conn;
+        public static OracleConnection connAdmin = new OracleConnection(@"Data Source=MONEY;Initial Catalog=HotelManagementSystem3;Integrated Security=True");
+
+
+        public DbConnectionOrcl()
+        {
+        }
+
+        /// <summary>
+        /// @param sql
+        /// </summary>
+        public void ThucThi(string sql)
+        {
+            OracleConnection conn = DbConnectionOrcl.conn;
+            try
+            {
+                conn.Open();
+                OracleCommand cmd = new OracleCommand(sql, conn);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+
+            }
+            finally { conn.Close(); }
+        }
+
+        /// <summary>
+        /// @param sql
+        /// </summary>
+        public DataTable LayDanhSach(string sql)
+        {
+            DataTable dt = new DataTable();
+            OracleConnection conn = DbConnectionOrcl.conn;
+            try
+            {
+                conn.Open();
+                OracleDataAdapter adapter = new OracleDataAdapter(sql, conn);
+                adapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally { conn.Close(); }
+            return dt;
+        }
+
+    }
+}
